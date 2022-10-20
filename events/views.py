f04c5d504ada5d6260fa00627143ef2c19471acc
Frederic THOUVENOT <frederic.tnt78@gmail.com>
2022-10-20 18:57:18 +0200

    API Client + Filtering v1.1

--- events/views.py
+++ events/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import DjangoModelPermissions
+from django_filters import rest_framework as df
 
 from events.models import Client
 from events.serializers import ClientSerializer
@@ -16,8 +17,19 @@ class PresentationView(LoginRequiredMixin, View):
         return render(request, self.template_name)
 
 
+class ClientFilter(df.FilterSet):
+    min_id = df.NumberFilter(field_name='id', lookup_expr='gte')
+    max_id = df.NumberFilter(field_name='id', lookup_expr='lte')
+
+    class Meta:
+        model = Client
+        fields = ['id', 'last_name', 'first_name', 'sales_contact']
+
+
 class ClientViewset(ModelViewSet):
 
     serializer_class = ClientSerializer
     permission_classes = [DjangoModelPermissions, IsClientSalesContact]
     queryset = Client.objects.all()
+    filter_backends = [df.DjangoFilterBackend]
+    filterset_class = ClientFilter
