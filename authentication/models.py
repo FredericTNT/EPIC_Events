f37a300513bdb7bb6ace4c221ecea8dc62bc0690
Frederic THOUVENOT <frederic.tnt78@gmail.com>
2022-10-23 13:18:13 +0200

    User/Group relation one2many

--- authentication/models.py
+++ authentication/models.py
@@ -1,14 +1,19 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,Group
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
 class User(AbstractUser):
-    """ Utilisateur générique django identification email"""
+    """
+    Utilisateur générique django
+    - identification email
+    - modification de la relation many2many user/group en une relation one2many
+    """
     email = models.EmailField(unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+    groups = models.ForeignKey(to=Group, on_delete=models.SET_NULL, null=True, blank=True)
 
 
 class Profile(models.Model):
