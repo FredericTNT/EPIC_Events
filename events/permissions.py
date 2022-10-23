83722dcefdc60ab6076444c19036e148a3d10021
Frederic THOUVENOT <frederic.tnt78@gmail.com>
2022-10-23 12:43:11 +0200

    API Client + Filtering v1.3

--- events/permissions.py
+++ events/permissions.py
@@ -1,14 +1,9 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class IsClientSalesContact(BasePermission):
+class IsClientSalesContact(LoginRequiredMixin, BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        """
-        Vérifier si l'utilisateur est authentifié et pour les méthodes autres que SAFE_METHODS (GET, HEAD, OPTIONS)
-        si l'utilisateur est administrateur ou le contact commercial du client
-        """
-        if (request.method in SAFE_METHODS) or request.user.is_superuser:
-            return request.user.is_authenticated
-        return (obj.sales_contact == request.user) and request.user.is_authenticated
-
+        return bool(request.method in SAFE_METHODS or request.user.is_superuser or
+                    not obj.sales_contact or obj.sales_contact == request.user)
