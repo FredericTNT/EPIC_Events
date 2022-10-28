ed64107dd4aa93cd4797ce2a6b5fa7b1df101030
FredericTNT <frederic.TNT78@gmail.com>
2022-10-28 22:11:58 +0200

    Refactor EPIC & Upgrade Requirements

--- epic/asgi.py
+++ epic/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for EPIC project.
+ASGI config for epic project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EPIC.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'epic.settings')
 
 application = get_asgi_application()
